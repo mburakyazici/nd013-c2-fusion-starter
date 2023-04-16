e4dc8e7841b9f406dde153957a5b8379c54bad0a
unknown <mahmutburakyazici@gmail.com>
2023-04-16 04:52:13 +0300

    step2 implementation

--- student/trackmanagement.py
+++ student/trackmanagement.py
@@ -35,20 +35,20 @@ class Track:
         # - initialize track state and track score with appropriate values
         ############
 
-        self.x = np.matrix([[49.53980697],
-                        [ 3.41006279],
-                        [ 0.91790581],
-                        [ 0.        ],
-                        [ 0.        ],
-                        [ 0.        ]])
-        self.P = np.matrix([[9.0e-02, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 9.0e-02, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 6.4e-03, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 2.5e+03, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 2.5e+03, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 2.5e+01]])
-        self.state = 'confirmed'
-        self.score = 0
+        px = meas.sensor.sens_to_veh * np.vstack([*meas.z, 1])
+        self.x = np.zeros((params.dim_state, 1))
+        self.x[: 3] = px[: 3]
+        self.P = np.zeros((params.dim_state, params.dim_state))
+        self.P[: 3, : 3] = meas.sensor.sens_to_veh[: 3, : 3] * meas.R * meas.sensor.sens_to_veh[: 3, : 3].T
+        self.P[3, 3] = params.sigma_p44 ** 2
+        self.P[4, 4] = params.sigma_p55 ** 2
+        self.P[5, 5] = params.sigma_p66 ** 2
+        self.x = np.asmatrix(self.x)
+        self.P = np.asmatrix(self.P)
+
+        self.state = 'initialized'
+        self.score = 1.0 / params.window
+
         
         ############
         # END student code
@@ -107,9 +107,15 @@ class Trackmanagement:
             if meas_list: # if not empty
                 if meas_list[0].sensor.in_fov(track.x):
                     # your code goes here
-                    pass 
+                    track.score -= 1.0 / params.window
 
         # delete old tracks   
+        for track in self.track_list:
+            if track.state == 'confirmed' and  track.score < params.delete_threshold:
+                self.delete_track(track)
+            if np.max(track.P[0 : 2, 0 : 2]) > params.max_P:
+                self.delete_track(track)
+
 
         ############
         # END student code
@@ -140,7 +146,11 @@ class Trackmanagement:
         # - set track state to 'tentative' or 'confirmed'
         ############
 
-        pass
+        track.score += 1.0 / params.window
+        if track.score >= params.confirmed_threshold:
+            track.state = 'confirmed'
+        elif track.score >= params.delete_threshold:
+            track.state = 'tentative'
         
         ############
         # END student code
